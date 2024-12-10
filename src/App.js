063f8b57726977faedc72fa96edd5b9822eec95a
mdmeeran90 <meeranfreek2193@gmail.com>
2024-12-10 14:34:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import ProductDetails from './pages/ProductDetails';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cart from './pages/Cart';
 
-function App() {
+function Apps() {
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <ToastContainer theme='dark' position="top-center" />
+          <Header cartItems={cartItems} />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Home />} />
+            <Route path='/product/:id' element={<ProductDetails cartItems={cartItems} setCartItems={setCartItems} />} />
+            <Route path='/cart' element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
+          </Routes>
+        </div>
+      </Router>
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Apps;
