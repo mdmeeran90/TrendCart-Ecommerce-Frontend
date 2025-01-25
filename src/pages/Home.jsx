4548678b68787bcc972e626edd9b339ae489c55b
Mohamed Meeran <148966222+mdmeeran90@users.noreply.github.com>
2025-01-25 09:48:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,34 @@
 import { Fragment, useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { useSearchParams } from "react-router-dom";
+import product from "../data/product.js";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_API_URL + '/products')
-      .then(res => res.json())
-      .then(res => setProducts(res.products));
+    try {
+      fetch(process.env.REACT_APP_API_URL + "/products")
+        .then((res) => res.json())
+        .then((res) => setProducts(res.products));
+  
+    } catch (error) {
+      console.error(error.message);
+    }
   }, [searchParams]);
 
-  return <Fragment>
-    <h1 id="products_heading">Latest Products</h1>
+  return (
+    <Fragment>
+      <h1 id="products_heading">Latest Products</h1>
 
-    <section id="products" className="container mt-5">
-      <div className="row">
-        {products.map((product) => <ProductCard product={product} />)}
-      </div>
-    </section>
-  </Fragment>
+      <section id="products" className="container mt-5">
+        <div className="row">
+          {products.length > 0
+            ? products.map((product,index) => <ProductCard  key={index} product={product} />)
+            : product.map((product,index) => <ProductCard key={index}  product={product} />)}
+        </div>
+      </section>
+    </Fragment>
+  );
 }
