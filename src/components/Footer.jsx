75c7ec50299a27d6b14a84f65defb0f816e1a77e
Mohamed Meeran <148966222+mdmeeran90@users.noreply.github.com>
2025-02-09 13:40:57 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return <footer className="py-1 bg-dark">
-    <p className="text-center text-white mt-1">
-      TrendCart - 2024, All Rights Reserved
+    <p className="text-center text-white mt-1 absolute bottom-1">
+      TrendCart - 2025, All Rights Reserved
     </p>
   </footer>
 }
