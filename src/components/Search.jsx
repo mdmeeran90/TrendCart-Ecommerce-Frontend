e40db98af5cd473081dcd86ceb4554211932de69
Mohamed Meeran <148966222+mdmeeran90@users.noreply.github.com>
2025-02-18 00:44:29 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,31 +1,27 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 export default function Search() {
-    const [keyword, setKeyword] = useState("");
+    const [keyword, setKeyword]  = useState("");
     const navigate = useNavigate();
 
     const searchHandler = () => {
-        if (keyword.trim()) {
-            navigate('/search?keyword='+keyword);
-        }
-    };
+        navigate('/search?keyword='+keyword)
+    }
 
-    return (
-        <div className="input-group">
-            <input
-                type="text"
-                id="search_field"
-                value={keyword}
-                onChange={(e) => setKeyword(e.target.value)}
-                className="form-control"
-                placeholder="Enter Product Name ..."
-            />
-            <div className="input-group-append">
-                <button onClick={searchHandler} id="search_btn" className="btn">
+    return   <div className="input-group">
+                <input
+                    type="text"
+                    id="search_field"
+                    onChange={(e) => setKeyword(e.target.value)}
+                    className="form-control"
+                    onBlur={searchHandler}
+                    placeholder="Enter Product Name ..."
+                />
+                <div className="input-group-append">
+                    <button onClick={searchHandler} id="search_btn" className="btn">
                     <i className="fa fa-search" aria-hidden="true"></i>
-                </button>
+                    </button>
+                </div>
             </div>
-        </div>
-    );
 }
